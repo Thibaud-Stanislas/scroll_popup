753b42dfd80168815f0732586732e415362f9bf7
Thibaud-Stanislas <stanislas.thibaud@gmail.com>
2023-04-26 11:40:36 +0200

    scroll popup

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let lastScroll = 10;
+let afterScroll = 300;
+let beforeScroll = 1400;
 
 window.addEventListener("scroll", () => {
   if (window.scrollY < lastScroll) {
@@ -7,6 +9,31 @@ window.addEventListener("scroll", () => {
     navbar.style.height = "50px";
   }
 });
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < afterScroll) {
+    imgImprovise.style.opacity = "0";
+    imgImprovise.style.translate = "-200px";
+  } else {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.translate = "200px";
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < beforeScroll) {
+    popup.style.opacity = "0";
+    popup.style.translate = "400px";
+  } else {
+    popup.style.opacity = "1";
+    popup.style.translate = "-400px";
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.translate = "400px";
+});
+
 // Créer un événement au scroll
 
 // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
